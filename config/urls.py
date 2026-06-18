48ae4f7d81fbf9faf1f6041567a9457fd239659c
resolvedlogic-nsw <resolvedlogic@gmail.com>
2026-06-18 18:08:56 +1000

    instructions

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('meals.urls')),
+    path('guide/', TemplateView.as_view(template_name='guide.html'), name='guide'),
 ]
