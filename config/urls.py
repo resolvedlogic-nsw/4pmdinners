6770d9d92061a3d9c061b9e1e5688ade2497172c
resolvedlogic-nsw <resolvedlogic@gmail.com>
2026-06-28 19:46:16 +1000

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('meals.urls')),
     path('store/', include('store.urls')),
+    path('', include('meals.urls')),
+    
 ]
